a0063c58e2a7d2904a2b56d47bf64913ece259c1
shivanibhadauria <shivanibhadauria921997@gmail.com>
2025-09-27 20:37:17 +0530

    feat: Initialize express app and basic setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
